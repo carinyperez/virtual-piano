16da567eb5e48e4a11e55bb03df390737c9340b8
Cariny Perez <perezcariny@gmail.com>
2021-07-29 18:32:15 -0700

    Add html and css to the piano component and create a homepage

---
+++ src/components/piano.component.jsx
@@ -0,0 +1,77 @@
+import './piano.styles.scss'; 
+
+const Piano = () => {
+    return (
+        <div> 
+        <div className='white-canvas'>
+            <div className="key white" ></div> 
+            <div className="key black"></div> 
+            <div className="key white" ></div> 
+            <div className="key black long"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+
+
+            <div className="key white" ></div> 
+            <div className="key black"></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key black"></div>
+            <div className="key white" ></div> 
+            <div className="key white" ></div> 
+        </div>
+        </div>
+    )
+}
+
+export default Piano;
+
+
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,12 @@
+import Piano from '../components/piano.component.jsx'; 
+
+const HomePage  = () => {
+    return (
+        <div>
+        <h1>Virtual Piano</h1>
+            <Piano/>
+        </div>
+    )
+}
+
+export default HomePage;
